46a436640633803eeeee3a1840cb9ba124711182
Jonathan <jonathan@koloa-designs.com>
2017-08-06 22:09:48 -0400

    more control flow practice

--- main.go
+++ main.go
@@ -5,15 +5,29 @@ import (
 )
 
 func main() {
+	// for loop
 	for i:= 1; i <= 5; i++ {
 		fmt.Println(i)
 	}
 
+	// if else statements
 	if false {
 		fmt.Println("true")
 	} else if true {
 		fmt.Println("Second true")
 	} else {
 		fmt.Println("else true")
+	}
+
+	// sample switch statement
+	houseNumber := 2404
+
+	switch houseNumber {
+	case 2404:
+		fmt.Println("That's our house!")
+	case 2424:
+		fmt.Println("Our Neighbor's house!")
+	default:
+		fmt.Println("I don't know whose house that is...")
 	}
 }
