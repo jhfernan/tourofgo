b0ca151213e63bbff918d5d6ed87e39a337fbb58
Jonathan <jonathan@koloa-designs.com>
2017-08-06 22:02:56 -0400

    Started testing control flow statements

--- main.go
+++ main.go
@@ -2,34 +2,18 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"math"
 )
 
 func main() {
-	a := "Hello, world!"
-
-	fmt.Println(a)
-	fmt.Println(sayHello("Jonathan"))
-
-	// setting square root function variables
-	squareRoot, err := squareRoot(9)
-	if err != nil {
-		log.Fatal(err)
+	for i:= 1; i <= 5; i++ {
+		fmt.Println(i)
 	}
-	fmt.Println(squareRoot)
-}
 
-// sample functions
-func sayHello(name string) (greeting string) {
-	greeting = "Hello, " + name + "!"
-	return
-}
-
-// returning multiple values
-func squareRoot(x float64) (float64, error) {
-	if x < 0 {
-		return 0, fmt.Errorf("Can't have a square root of a negative number")
+	if false {
+		fmt.Println("true")
+	} else if true {
+		fmt.Println("Second true")
+	} else {
+		fmt.Println("else true")
 	}
-	return math.Sqrt(x), nil
 }
